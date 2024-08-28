4d7d13773842d90735a4154d1f28906c85727153
tom.jen <tom.jen@gallopwave.com>
2024-08-28 11:06:26 +0800

    convolution Seperable

---
+++ convolutionSeperable/convolutionSeparable.cu
@@ -0,0 +1,234 @@
+
+/* Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <helper_cuda.h>
+#include <cooperative_groups.h>
+
+namespace cg = cooperative_groups;
+#include "convolutionSeparable_common.h"
+
+#define MAX_KERNEL_LENGTH 128
+#define BLOCKDIM_X 16
+#define BLOCKDIM_Y 16
+#define RESULT_STEPS 4
+
+////////////////////////////////////////////////////////////////////////////////
+// Convolution kernel storage
+////////////////////////////////////////////////////////////////////////////////
+
+__constant__ float c_Kernel[MAX_KERNEL_LENGTH];
+
+extern "C" void setConvolutionKernel(float *h_Kernel, int kernelSize) {
+    cudaMemcpyToSymbol(c_Kernel, h_Kernel, kernelSize * sizeof(float));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Row convolution filter
+////////////////////////////////////////////////////////////////////////////////
+__global__ void convolutionRowsKernel(float *d_Dst, float *d_Src, int imageW, int imageH, int rowsHaloSteps, int kernelRadius) {
+    // Handle to thread block group
+    cg::thread_block cta = cg::this_thread_block();
+
+    extern __shared__ float s_Data[];
+    int idx_x = blockDim.x * (blockIdx.x * RESULT_STEPS) + threadIdx.x; 
+    int idx_y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    // Load main data
+    #pragma unroll
+    for (int i = 0; i < RESULT_STEPS; i++) {
+        int localIdx_x = threadIdx.x + (i + rowsHaloSteps) * blockDim.x;
+        int localIdx_y = threadIdx.y;
+        int localIdx = localIdx_y * (RESULT_STEPS + 2 * rowsHaloSteps) * blockDim.x + localIdx_x;
+        
+        int dataIdx_x = idx_x + i * blockDim.x;
+        int dataIdx_y = idx_y;
+        int dataIdx = dataIdx_y * imageW + dataIdx_x;
+
+        s_Data[localIdx] = (dataIdx_x < imageW && dataIdx_y < imageH) ? d_Src[dataIdx] : 0;
+    }
+
+    // Load left halo
+    #pragma unroll
+    for (int i = 0; i < rowsHaloSteps; i++) {
+
+        int localIdx_x = threadIdx.x + i * blockDim.x;
+        int localIdx_y = threadIdx.y;
+        int localIdx = localIdx_y * (RESULT_STEPS + 2 * rowsHaloSteps) * blockDim.x + localIdx_x;
+
+        int dataIdx_x = idx_x - (rowsHaloSteps-i) * blockDim.x;
+        int dataIdx_y = idx_y;
+        int dataIdx = dataIdx_y * imageW + dataIdx_x;
+
+        s_Data[localIdx] = (dataIdx_x >= 0 && dataIdx_y < imageH) ? d_Src[dataIdx] : 0;
+    }
+    
+    // Load right halo
+    #pragma unroll
+    for (int i = 0; i < rowsHaloSteps; i++) {
+        int localIdx_x = threadIdx.x + (RESULT_STEPS + rowsHaloSteps + i) * blockDim.x;
+        int localIdx_y = threadIdx.y;
+        int localIdx = localIdx_y * (RESULT_STEPS + 2 * rowsHaloSteps) * blockDim.x + localIdx_x;
+        
+        int dataIdx_x = idx_x + (RESULT_STEPS + i) * blockDim.x;
+        int dataIdx_y = idx_y;
+        int dataIdx = dataIdx_y * imageW + dataIdx_x;
+
+        s_Data[localIdx] = (dataIdx_x < imageW && dataIdx_y < imageH) ? d_Src[dataIdx] : 0;
+    }
+
+    // Compute convolution and store results
+    cg::sync(cta);
+
+    #pragma unroll
+    for (int i = 0; i < RESULT_STEPS; i++) {
+        float sum = 0;
+
+        #pragma unroll
+        for (int j = -kernelRadius; j <= kernelRadius; j++) {
+            int localIdx_x = threadIdx.x + (i + rowsHaloSteps) * blockDim.x + j;
+            int localIdx_y = threadIdx.y;
+            int localIdx = localIdx_y * (RESULT_STEPS + 2 * rowsHaloSteps) * blockDim.x + localIdx_x;
+            sum += c_Kernel[kernelRadius - j] * s_Data[localIdx];
+        }
+
+        int dataIdx_x = idx_x + i * blockDim.x;
+        int dataIdx_y = idx_y;
+        int dataIdx = dataIdx_y * imageW + dataIdx_x;
+        if (dataIdx_x < imageW && dataIdx_y < imageH)
+            d_Dst[dataIdx] = sum;
+    }
+}
+
+extern "C" void convolutionRowsGPU(float *d_Dst, float *d_Src, int imageW, int imageH, int kernelSize) {
+
+    dim3 dimBlock(BLOCKDIM_X, BLOCKDIM_Y);
+    int stride = BLOCKDIM_X * RESULT_STEPS;
+    dim3 dimGrid((imageW + stride - 1) / (stride), 
+                 (imageH + BLOCKDIM_Y - 1) / BLOCKDIM_Y);
+
+    int kernelRadius = kernelSize / 2;
+    int rowsHaloSteps = kernelRadius / BLOCKDIM_X + 1; 
+
+    int sharedMemSize = BLOCKDIM_Y * (RESULT_STEPS + 2 * rowsHaloSteps) * BLOCKDIM_X * sizeof(float);
+    convolutionRowsKernel<<<dimGrid, dimBlock, sharedMemSize, 0>>>(d_Dst, d_Src, imageW, imageH, rowsHaloSteps, kernelRadius);
+    getLastCudaError("convolutionRowsKernel() execution failed\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Column convolution filter
+////////////////////////////////////////////////////////////////////////////////
+__global__ void convolutionColumnsKernel(float *d_Dst, float *d_Src, int imageW, int imageH, int colsHaloSteps, int kernelRadius) {
+    // Handle to thread block group
+    cg::thread_block cta = cg::this_thread_block();
+
+    extern __shared__ float s_Data[];
+    int idx_x = blockDim.x * blockIdx.x + threadIdx.x; 
+    int idx_y = blockDim.y * (blockIdx.y * RESULT_STEPS) + threadIdx.y;
+
+    // Load main data
+    #pragma unroll
+    for (int i = 0; i < RESULT_STEPS; i++) {
+        int localIdx_x = threadIdx.x;
+        int localIdx_y = threadIdx.y + (i + colsHaloSteps) * blockDim.y;
+        int localIdx = localIdx_y * blockDim.x + localIdx_x;
+        
+        int dataIdx_x = idx_x;
+        int dataIdx_y = idx_y + i * blockDim.y;
+        int dataIdx = dataIdx_y * imageW + dataIdx_x;
+
+        s_Data[localIdx] = (dataIdx_x < imageW && dataIdx_y < imageH) ? d_Src[dataIdx] : 0;
+    }
+
+    // Load top halo
+    #pragma unroll
+    for (int i = 0; i < colsHaloSteps; i++) {
+
+        int localIdx_x = threadIdx.x;
+        int localIdx_y = threadIdx.y + i * blockDim.y;
+        int localIdx = localIdx_y * blockDim.x + localIdx_x;
+
+        int dataIdx_x = idx_x;
+        int dataIdx_y = idx_y - (colsHaloSteps-i) * blockDim.y;
+        int dataIdx = dataIdx_y * imageW + dataIdx_x;
+
+        s_Data[localIdx] = (dataIdx_y >= 0 && dataIdx_x < imageW) ? d_Src[dataIdx] : 0;
+    }
+
+    // Load buttom halo
+    #pragma unroll
+    for (int i = 0; i < colsHaloSteps; i++) {
+        int localIdx_x = threadIdx.x;
+        int localIdx_y = threadIdx.y + (RESULT_STEPS + colsHaloSteps + i) * blockDim.y;
+        int localIdx = localIdx_y * blockDim.x + localIdx_x;
+
+        int dataIdx_x = idx_x;
+        int dataIdx_y = idx_y + (RESULT_STEPS + i) * blockDim.y;
+        int dataIdx = dataIdx_y * imageW + dataIdx_x;
+        
+        s_Data[localIdx] = (dataIdx_x < imageW && dataIdx_y < imageH) ? d_Src[dataIdx] : 0;
+    }
+
+    // Compute convolution and store results
+    cg::sync(cta);
+
+    #pragma unroll
+    for (int i = 0; i < RESULT_STEPS; i++) {
+        float sum = 0;
+
+        #pragma unroll
+        for (int j = -kernelRadius; j <= kernelRadius; j++) {
+            int localIdx_x = threadIdx.x;
+            int localIdx_y = threadIdx.y + (i + colsHaloSteps) * blockDim.y + j;
+            int localIdx = localIdx_y * blockDim.x + localIdx_x;
+            sum += c_Kernel[kernelRadius - j] * s_Data[localIdx];
+        }
+
+        int dataIdx_x = idx_x;
+        int dataIdx_y = idx_y + i * blockDim.y;
+        int dataIdx = dataIdx_y * imageW + dataIdx_x;
+        if (dataIdx_x < imageW && dataIdx_y < imageH) {
+            d_Dst[dataIdx] = sum;
+        }
+    }
+}
+
+extern "C" void convolutionColumnsGPU(float *d_Dst, float *d_Src, int imageW, int imageH, int kernelSize) {
+
+    dim3 dimBlock(BLOCKDIM_X, BLOCKDIM_Y);
+    int stride = BLOCKDIM_Y * RESULT_STEPS;
+    dim3 dimGrid((imageW + BLOCKDIM_X - 1) / (BLOCKDIM_X), 
+                 (imageH + stride - 1) / (stride));
+    printf("dimGrid: %d %d\n", dimGrid.x, dimGrid.y);
+    int kernelRadius = kernelSize / 2;
+    int colsHaloSteps = kernelRadius / BLOCKDIM_Y + 1; 
+
+    int sharedMemSize = (RESULT_STEPS + 2 * colsHaloSteps) * BLOCKDIM_Y * BLOCKDIM_X * sizeof(float);
+    convolutionColumnsKernel<<<dimGrid, dimBlock, sharedMemSize, 0>>>(d_Dst, d_Src, imageW, imageH, colsHaloSteps, kernelRadius);
+    getLastCudaError("convolutionColumnsGPU() execution failed\n");
+}
---
+++ convolutionSeperable/main.cpp
@@ -0,0 +1,148 @@
+// CUDA runtime
+#include <cuda_runtime.h>
+
+// Utilities and system includes
+#include <helper_functions.h>
+#include <helper_cuda.h>
+
+#include "convolutionSeparable_common.h"
+
+void generate_data(float *h_buffer, int imageH, int imageW)
+{
+    for (int row = 0; row < imageH; row++) {
+        for (int col = 0; col < imageW; col++) {
+
+            h_buffer[row * imageW + col] = static_cast<float>(rand()) / RAND_MAX * 10.0f;
+            // h_buffer[row * imageW + col] = 1.f;
+        }
+    }
+}
+
+/* Generates Bi-symetric Gaussian Filter */
+void generate_1D_kernel(float *h_kernel, int kernelSize, float sigma)
+{
+    float sumKernel = 0.f; //for normalization
+    for (int i = -kernelSize / 2; i <= kernelSize / 2; i++)
+    {
+        float kernelValue = expf(-(float)(i * i) / (2.f * sigma * sigma));
+        h_kernel[i + kernelSize / 2] = kernelValue;
+        sumKernel += kernelValue;
+    }
+
+    // normalization
+    float normalizationFactor = 1.f / sumKernel;
+    for (int i = -kernelSize / 2; i <= kernelSize / 2; i++)
+        h_kernel[i + kernelSize / 2] *= normalizationFactor;
+}
+
+int main(int argc, char **argv) {
+    // start logs
+    printf("[%s] - Starting...\n", argv[0]);
+
+    // buffers
+    float *h_input, *h_outputGPU;
+    float *d_input, *d_buffer, *d_output;  // d_buffer is used for intermediate result after row convolution
+    float *h_kernel;
+
+    // image size and kernel size
+    const int imageW = 1920;
+    const int imageH = 1080;
+    const int kernelSize = 7;
+    int bufferSize = imageW * imageH * sizeof(float);
+
+    // initialize timer
+    StopWatchInterface *hTimer;
+    sdkCreateTimer(&hTimer);
+
+    // initialize cuda event
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // allocate host memories
+    h_input = (float *)malloc(bufferSize);
+    h_outputGPU = (float *)malloc(bufferSize);
+    h_kernel = (float *)malloc(kernelSize * sizeof(float));
+
+    // allocate gpu memories
+    checkCudaErrors(cudaMalloc((void **)&d_input, bufferSize));
+    checkCudaErrors(cudaMalloc((void **)&d_output, bufferSize));
+    checkCudaErrors(cudaMalloc((void **)&d_buffer, bufferSize));
+
+    // generate data and filter
+    generate_data(h_input, imageH, imageW);
+    generate_1D_kernel(h_kernel, kernelSize, 1.f);
+
+    // set kernel into global memory of GPU
+    setConvolutionKernel(h_kernel, kernelSize);
+
+    // gpu memory copy
+    checkCudaErrors(cudaMemcpy(d_input, h_input, bufferSize, cudaMemcpyHostToDevice));
+
+    // reset timer
+    checkCudaErrors(cudaDeviceSynchronize());
+    sdkResetTimer(&hTimer);
+    sdkStartTimer(&hTimer);
+    cudaEventRecord(start);
+
+    // convolution
+    convolutionRowsGPU(d_buffer, d_input, imageW, imageH, kernelSize);
+    convolutionColumnsGPU(d_output, d_buffer, imageW, imageH, kernelSize);
+
+    // event record
+    cudaEventRecord(stop);
+    checkCudaErrors(cudaEventSynchronize(stop));
+
+    // calculate elapsed time
+    checkCudaErrors(cudaDeviceSynchronize());
+    sdkStopTimer(&hTimer);
+
+    // print elapsed time by cuda event
+    float elapsed_time_msed = 0.f;
+    cudaEventElapsedTime(&elapsed_time_msed, start, stop);
+    printf("CUDA event estimated - elapsed %.3f ms \n", elapsed_time_msed);
+
+    // print elapsed time by sdk timer
+    float elapsed_timer_gpu = sdkGetTimerValue(&hTimer);
+    printf("Processing Time: %.6f ms\n", elapsed_timer_gpu);
+
+    // back to host
+    printf("\nReading back GPU results...\n\n");
+    checkCudaErrors(cudaMemcpy(h_outputGPU, d_output, bufferSize, cudaMemcpyDeviceToHost));
+
+    printf("Checking the results...\n");
+
+    float *h_buffer, *h_outputCPU;
+    h_buffer = (float *)malloc(bufferSize);
+    h_outputCPU = (float *)malloc(bufferSize);
+
+    printf(" ...running convolutionRowCPU()\n");
+    convolutionRowCPU(h_buffer, h_input, h_kernel, imageW, imageH, kernelSize);
+    printf(" ...running convolutionColumnCPU()\n");
+    convolutionColumnCPU(h_outputCPU, h_buffer, h_kernel, imageW, imageH, kernelSize);
+
+    printf(" ...comparing the results\n");
+    double sum = 0, delta = 0;
+
+    for (unsigned i = 0; i < imageW * imageH; i++) {
+        delta +=
+            (h_outputGPU[i] - h_outputCPU[i]) * (h_outputGPU[i] - h_outputCPU[i]);
+        sum += h_outputCPU[i] * h_outputCPU[i];
+    }
+
+    double L2norm = sqrt(delta / sum);
+    printf(" ...Relative L2 norm: %E\n\n", L2norm);
+    printf("Shutting down...\n");
+
+    checkCudaErrors(cudaFree(d_input));
+    checkCudaErrors(cudaFree(d_buffer));
+    checkCudaErrors(cudaFree(d_output));
+    free(h_input);
+    free(h_outputGPU);
+    free(h_kernel);
+    free(h_buffer);
+    free(h_outputCPU);
+    sdkDeleteTimer(&hTimer);
+
+    return 0;
+}
